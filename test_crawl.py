51198e1465c5ac0d7cea1c0920aeb8428810b5b9
Ifaz Howlader <ifaz.howl@gmail.com>
2025-08-18 00:30:49 -0400

    I think you can tell the difference between the testcases I wrote and what the AI has written. There is wisdom to be picked up from here.

--- test_crawl.py
+++ test_crawl.py
@@ -27,6 +27,29 @@ class TestCrawl(unittest.TestCase):
         expected = "blog.boot.dev/path"
         self.assertEqual(actual, expected)
     
+    def test_removes_protocol(self):
+        self.assertEqual(normalize_url("https://blog.boot.dev/path"), "blog.boot.dev/path")
+        self.assertEqual(normalize_url("http://blog.boot.dev/path"), "blog.boot.dev/path")
+
+    def test_removes_trailing_slash(self):
+        self.assertEqual(normalize_url("https://blog.boot.dev/path/"), "blog.boot.dev/path")
+
+    def test_handles_mixed_case(self):
+        self.assertEqual(normalize_url("https://BLOG.boot.DEV/path"), "blog.boot.dev/path")
+
+    def test_removes_query_params(self):
+        self.assertEqual(normalize_url("https://blog.boot.dev/path?query=1"), "blog.boot.dev/path")
+
+    def test_removes_fragments(self):
+        self.assertEqual(normalize_url("https://blog.boot.dev/path#section"), "blog.boot.dev/path")
+
+    def test_handles_different_protocols(self):
+        self.assertEqual(normalize_url("http://blog.boot.dev/path"), "blog.boot.dev/path")
+        self.assertEqual(normalize_url("https://blog.boot.dev/path"), "blog.boot.dev/path")
+
+    def test_handles_empty_path(self):
+        self.assertEqual(normalize_url("https://blog.boot.dev"), "blog.boot.dev")
+        self.assertEqual(normalize_url("https://blog.boot.dev/"), "blog.boot.dev")
 
 if __name__ == "__main__":
     unittest.main()
