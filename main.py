7df1fa6da8dbb7f84490bf310868af1510f75b60
Ifaz Howlader <ifaz.howl@gmail.com>
2025-08-18 12:46:58 -0400

    updating main.py. indicating the start of crawling a particular website.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import sys
+
 def main():
-    print("Hello from webcrawler!")
+    if (len(sys.argv) < 2):
+        print("no website provided")
+        sys.exit(1)
+    elif (len(sys.argv) > 2):
+        print("too many arguments")
+        sys.exit(1)
+    else:
+        print(f"starting crawl of: {sys.argv[0]}")
 
 
 if __name__ == "__main__":
     main()
+
